9c71e4fe164a87a5d8911faf8cca97f677387072
Ted Maranets <tdmaranets9@gmail.com>
2015-05-22 06:33:58 -0400

    Update ProjectileEntity.java
    added some stuff for projectiles

--- src/org/amityregion5/qxrz/server/world/entity/ProjectileEntity.java
+++ src/org/amityregion5/qxrz/server/world/entity/ProjectileEntity.java
@@ -2,10 +2,19 @@ package org.amityregion5.qxrz.server.world.entity;
 
 public abstract class ProjectileEntity extends GameEntity
 {
-
+	private final int PROJECTILE_SIZE = 1;
 	public ProjectileEntity()
 	{
 		// TODO Auto-generated constructor stub
+	}
+	
+	public boolean update(double tSinceUpdate, Landscape surroundings)
+	{
+		
+	}
+	public RectangleHitbox getHitBox()
+	{
+		return new RectangleHitbox(new Rectangle2D.Double(pos.getX(),pos.getY(),PROJECTILE_SIZE,PROJECTILE_SIZE));
 	}
 
 }
