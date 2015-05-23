cf7c2c8a7d4249783a4cd86ad4c920c33b7d2da4
mkirsch42 <mkirsch42@gmail.com>
2015-05-23 18:32:11 -0400

    Bring DebugConstants back I need to see if there is some way to keep this in the repo but not update it.

---
+++ src/org/amityregion5/qxrz/server/DebugConstants.java
@@ -0,0 +1,11 @@
+package org.amityregion5.qxrz.server;
+
+public final class DebugConstants
+{
+
+	public static final int UPDATE_RATE = 15;
+	public static final int DEBUG_FPS = 60;
+	public static final boolean DEBUG_PATH = false;
+	public static final double PATH_LEN = 2;
+	
+}
