151a871a3bdae36a6bdf8981bb2a70b3160a2147
SergeySave <sergeysav.nn@gmail.com>
2015-05-29 09:03:15 -0400

    Network Input Data (NID)

--- src/org/amityregion5/qxrz/client/ui/screen/GameScreen.java
+++ src/org/amityregion5/qxrz/client/ui/screen/GameScreen.java
@@ -3,7 +3,10 @@ package org.amityregion5.qxrz.client.ui.screen;
 import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
 import org.amityregion5.qxrz.client.ui.MainGui;
+import org.amityregion5.qxrz.common.control.NetworkInputData;
+import org.amityregion5.qxrz.common.control.NetworkInputMasks;
 import org.amityregion5.qxrz.common.ui.DrawableObject;
 import org.amityregion5.qxrz.common.ui.Viewport;
 import org.amityregion5.qxrz.server.Game;
@@ -31,23 +34,23 @@ public class GameScreen extends AbstractScreen
 		g.setColor(Color.WHITE);
 		g.fillRect(0,0, windowData.getWidth(), windowData.getHeight());
 
-		if (windowData.getKeysDown().contains(KeyEvent.VK_W)) {
+		if (windowData.getKeysDown().contains(KeyEvent.VK_I)) {
 			vp.yCenter-=100;
 		}			
-		if (windowData.getKeysDown().contains(KeyEvent.VK_A)) {
+		if (windowData.getKeysDown().contains(KeyEvent.VK_J)) {
 			vp.xCenter-=100;
 		}
-		if (windowData.getKeysDown().contains(KeyEvent.VK_S)) {
+		if (windowData.getKeysDown().contains(KeyEvent.VK_K)) {
 			vp.yCenter+=100;
 		}			
-		if (windowData.getKeysDown().contains(KeyEvent.VK_D)) {
+		if (windowData.getKeysDown().contains(KeyEvent.VK_L)) {
 			vp.xCenter+=100;
 		}
-		if (windowData.getKeysDown().contains(KeyEvent.VK_Q)) {
+		if (windowData.getKeysDown().contains(KeyEvent.VK_U)) {
 			vp.height+=(2/1.5) * 100;
 			vp.width+=2 * 100;
 		}			
-		if (windowData.getKeysDown().contains(KeyEvent.VK_E) && vp.height > 600) {
+		if (windowData.getKeysDown().contains(KeyEvent.VK_O) && vp.height > 600) {
 			vp.height-=(2/1.5) * 100;
 			vp.width-=2 * 100;
 		}
@@ -69,7 +72,22 @@ public class GameScreen extends AbstractScreen
 			}
 		}
 		
-		
+		//Do network input data stuff
+		{
+			NetworkInputData nid = new NetworkInputData();
+			
+			nid.set(NetworkInputMasks.W, windowData.getKeysDown().contains(KeyEvent.VK_W));
+			nid.set(NetworkInputMasks.A, windowData.getKeysDown().contains(KeyEvent.VK_A));
+			nid.set(NetworkInputMasks.S, windowData.getKeysDown().contains(KeyEvent.VK_S));
+			nid.set(NetworkInputMasks.D, windowData.getKeysDown().contains(KeyEvent.VK_D));
+			nid.set(NetworkInputMasks.M1, windowData.getMiceDown().contains(MouseEvent.BUTTON1));
+			nid.set(NetworkInputMasks.R, windowData.getKeysDown().contains(KeyEvent.VK_R));
+			nid.set(NetworkInputMasks.SPACE, windowData.getKeysDown().contains(KeyEvent.VK_SPACE));
+			nid.set(NetworkInputMasks.COMMA, windowData.getKeysDown().contains(KeyEvent.VK_COMMA));
+			nid.set(NetworkInputMasks.PERIOD, windowData.getKeysDown().contains(KeyEvent.VK_PERIOD));
+			
+			//TODO: send somewhere
+		}
 	}
 
 	@Override
