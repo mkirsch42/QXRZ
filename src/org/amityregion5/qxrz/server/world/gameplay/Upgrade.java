bf510f11d47996e620e4f6a29455bc54fea8a753
zirvis <tdmaranets9@gmail.com>
2015-06-02 11:49:22 -0400

    worked more on Player and gameplay interactions with weapons and stuff

--- src/org/amityregion5/qxrz/server/world/gameplay/Upgrade.java
+++ src/org/amityregion5/qxrz/server/world/gameplay/Upgrade.java
@@ -2,13 +2,24 @@ package org.amityregion5.qxrz.server.world.gameplay;
 
 public class Upgrade {
 	private Player pl;
-	private String type;
+	private String type
 	public Upgrade()
 	{
 		
 	}
-	public Upgrade(String choice)
+	public Upgrade(String type)
 	{
-		
+		if (type.equals("rof"))
+		{
+			
+		}
+		else if (type.equals("cmax"))
+		{
+			
+		}
+		else if (type.equals("maxammo"))
+		{
+			pl.getEquip();
+		}
 	}
 }
