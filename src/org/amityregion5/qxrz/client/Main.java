559c4a6b7ca794b2fbdd3db3e79904191ecef94e
Kelvin Zhang <zhangxp1998@gmail.com>
2015-06-05 10:49:50 -0400

    format

--- src/org/amityregion5/qxrz/client/Main.java
+++ src/org/amityregion5/qxrz/client/Main.java
@@ -22,7 +22,7 @@ public class Main
 	{
 		List<ServerInfo> queryServers = new ArrayList<ServerInfo>();
 		List<ChatMessage> chatMessages = new ArrayList<ChatMessage>();
-		
+
 		ClientNetworkManager manager = new ClientNetworkManager();
 
 		manager.attachEventListener(new NetEventListener()
@@ -32,66 +32,69 @@ public class Main
 			public void newNode(AbstractNetworkNode server)
 			{
 				System.out.println("yay server responded");
-				if (queryServers.contains(server)) {
-					queryServers.set(queryServers.indexOf((ServerInfo)server), (ServerInfo)server);
-				} else {
-					queryServers.add((ServerInfo)server);
+				if (queryServers.contains(server))
+				{
+					queryServers.set(queryServers.indexOf((ServerInfo) server),
+							(ServerInfo) server);
+				} else
+				{
+					queryServers.add((ServerInfo) server);
 				}
 
 				// This will be called when a new server makes itself known.
 
-				// You should cast the AbstractNetworkNode to a ServerInfo before using
+				// You should cast the AbstractNetworkNode to a ServerInfo
+				// before using
 			}
 
 			@Override
 			public void dataReceived(NetworkNode from, Serializable payload)
 			{
 				System.out.println(from.getSocketAddress());
-				if(payload instanceof ChatMessage)
+				if (payload instanceof ChatMessage)
 				{
-					chatMessages.add(0, (ChatMessage)payload);
-					//queryServers.add((ServerInfo)payload);
-					/* call gui functions here
-					 * like gui.addServer(from, ServerInfo) or something
-					 * and then addServer() would add that server to the menu
+					chatMessages.add(0, (ChatMessage) payload);
+					// queryServers.add((ServerInfo)payload);
+					/*
+					 * call gui functions here like gui.addServer(from,
+					 * ServerInfo) or something and then addServer() would add
+					 * that server to the menu
 					 */
 				}
 			}
 		});
 		manager.start();
 		manager.broadcastQuery();
-		//Create a gui object
-		/* TODO this should take the manager in its constructor
-		 * then you can call
-		 * manager.broadcastQuery()
-		 * and
-		 * manager.connect(address) when the user chooses a server.
+		// Create a gui object
+		/*
+		 * TODO this should take the manager in its constructor then you can
+		 * call manager.broadcastQuery() and manager.connect(address) when the
+		 * user chooses a server.
 		 */
-		MainGui gui = new MainGui(manager, queryServers, chatMessages); 
+		MainGui gui = new MainGui(manager, queryServers, chatMessages);
 
-		//Called when the JRE closes
-		Runtime.getRuntime().addShutdownHook(new Thread(()->{
+		// Called when the JRE closes
+		Runtime.getRuntime().addShutdownHook(new Thread(() ->
+		{
 			System.out.println("Start Shutdown Hook");
-			//Clean up gui stuff
-			gui.closeGame();
-			
-			//Send a disconnect notification packet to the server
-			manager.sendObject(new DisconnectNotification());
-			
+			// Clean up gui stuff
+				gui.closeGame();
 
-			//manager.close();
-		}, "Shutdown Hook thread"));
+				// Send a disconnect notification packet to the server
+				manager.sendObject(new DisconnectNotification());
 
+				// manager.close();
+			}, "Shutdown Hook thread"));
 
-		//Show the gui
+		// Show the gui
 		gui.show();
 
-		//Do loading stuffs
+		// Do loading stuffs
 		{
 			AssetManager.loadAssets();
 		}
 
-		//Set the screen to the main menu screen
+		// Set the screen to the main menu screen
 		gui.setCurrentScreen(new MainMenuScreen(gui));
 	}
 }
