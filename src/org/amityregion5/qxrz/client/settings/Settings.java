91a196f0d9dfca4671433c30585a80a7b831b980
SergeySave <sergeysav.nn@gmail.com>
2015-06-12 15:34:27 -0400

    Finished Settings

--- src/org/amityregion5/qxrz/client/settings/Settings.java
+++ src/org/amityregion5/qxrz/client/settings/Settings.java
@@ -12,55 +12,92 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutput;
 import java.io.ObjectOutputStream;
 import java.io.OutputStream;
-import java.io.Serializable;
+import java.util.Arrays;
 import java.util.HashMap;
 
-public class Settings implements Serializable
+public class Settings
 {
-	private static final long serialVersionUID = 2132659756229359129L;
+	private static HashMap<String, Object> defaultSettings;
 
-	private HashMap<String, Object> storage;
+	private transient HashMap<String, Object> renderSettings;
+	private HashMap<String, Integer> storage;
 
-	@SuppressWarnings("unchecked")
+	static {
+		defaultSettings = new HashMap<String, Object>();
+		defaultSettings.put("RenderQuality", RenderingHints.VALUE_RENDER_SPEED);
+		defaultSettings.put("TextAntialias", RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+		defaultSettings.put("Dither", RenderingHints.VALUE_DITHER_ENABLE);
+		defaultSettings.put("Antialias", RenderingHints.VALUE_ANTIALIAS_ON);
+		defaultSettings.put("FactionalMetrics", RenderingHints.VALUE_FRACTIONALMETRICS_ON);
+		defaultSettings.put("AlphaInterpolation", RenderingHints.VALUE_ALPHA_INTERPOLATION_QUALITY);
+		defaultSettings.put("ColorRenderQuality", RenderingHints.VALUE_COLOR_RENDER_QUALITY);
+		defaultSettings.put("Stroke", RenderingHints.VALUE_STROKE_PURE);
+	}
+
 	public Settings()
 	{
+		load();
+
+		putAllIfAbsent(renderSettings, defaultSettings);
+		retainKeys(renderSettings, defaultSettings);
+	}
+
+	@SuppressWarnings("unchecked")
+	public void load() {
 		try{
 			InputStream file = new FileInputStream("QXRZ_Settings.ini");
 			InputStream buffer = new BufferedInputStream(file);
 			ObjectInput input = new ObjectInputStream (buffer);
 
-			storage = (HashMap<String, Object>)input.readObject();
+			storage = (HashMap<String, Integer>)input.readObject();
 			input.close();
+			
+			renderSettings = new HashMap<String, Object>();
+			readIntToObj(renderSettings, storage, "RenderQuality", RenderingHints.VALUE_RENDER_QUALITY, RenderingHints.VALUE_RENDER_SPEED);
+			readIntToObj(renderSettings, storage, "TextAntialias", RenderingHints.VALUE_TEXT_ANTIALIAS_ON, RenderingHints.VALUE_TEXT_ANTIALIAS_OFF);
+			readIntToObj(renderSettings, storage, "Dither", RenderingHints.VALUE_DITHER_ENABLE, RenderingHints.VALUE_DITHER_DISABLE);
+			readIntToObj(renderSettings, storage, "Antialias", RenderingHints.VALUE_ANTIALIAS_ON, RenderingHints.VALUE_ANTIALIAS_OFF);
+			readIntToObj(renderSettings, storage, "FactionalMetrics", RenderingHints.VALUE_FRACTIONALMETRICS_ON, RenderingHints.VALUE_FRACTIONALMETRICS_OFF);
+			readIntToObj(renderSettings, storage, "AlphaInterpolation", RenderingHints.VALUE_ALPHA_INTERPOLATION_QUALITY, RenderingHints.VALUE_ALPHA_INTERPOLATION_SPEED);
+			readIntToObj(renderSettings, storage, "ColorRenderQuality", RenderingHints.VALUE_COLOR_RENDER_QUALITY, RenderingHints.VALUE_COLOR_RENDER_SPEED);
+			readIntToObj(renderSettings, storage, "Stroke", RenderingHints.VALUE_STROKE_PURE, RenderingHints.VALUE_STROKE_NORMALIZE);
+			storage = null;
 		}catch (IOException | ClassNotFoundException e){
-			storage = new HashMap<String, Object>();
+			renderSettings = defaultSettings;;
 		}
-
-		HashMap<String, Object> defaultStorage = new HashMap<String, Object>();
-		defaultStorage.put("RenderQuality", RenderingHints.VALUE_RENDER_SPEED);
-		defaultStorage.put("TextAntialias", RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
-		defaultStorage.put("Dither", RenderingHints.VALUE_DITHER_ENABLE);
-		defaultStorage.put("Antialias", RenderingHints.VALUE_ANTIALIAS_ON);
-		defaultStorage.put("FactionalMetrics", RenderingHints.VALUE_FRACTIONALMETRICS_ON);
-		defaultStorage.put("AlphaInterpolation", RenderingHints.VALUE_ALPHA_INTERPOLATION_QUALITY);
-		defaultStorage.put("ColorRenderQuality", RenderingHints.VALUE_COLOR_RENDER_QUALITY);
-		defaultStorage.put("Stroke", RenderingHints.VALUE_STROKE_PURE);
-
-		putAllIfAbsent(storage, defaultStorage);
-		retainKeys(storage, defaultStorage);
 	}
 
+	public void readIntToObj(HashMap<String, Object> rS, HashMap<String, Integer> so, String key, Object... stuff) {
+		if (so.get(key) != null) {
+			rS.put(key, stuff[so.get(key).intValue()]);
+		}
+	}
+
+	public void writeIntFromObj(HashMap<String, Object> rS, HashMap<String, Integer> so, String key, Object... stuff) {
+		if (rS.get(key) != null) {
+			so.put(key, Arrays.asList(stuff).indexOf(rS.get(key)));
+		}
+	}
+	
 	public void save() {
 		try{
 			OutputStream file = new FileOutputStream("QXRZ_Settings.ini");
 			OutputStream buffer = new BufferedOutputStream(file);
 			ObjectOutput output = new ObjectOutputStream(buffer);
-			
+
+			storage = new HashMap<String, Integer>();
+			writeIntFromObj(renderSettings, storage, "RenderQuality", RenderingHints.VALUE_RENDER_QUALITY, RenderingHints.VALUE_RENDER_SPEED);
+			writeIntFromObj(renderSettings, storage, "TextAntialias", RenderingHints.VALUE_TEXT_ANTIALIAS_ON, RenderingHints.VALUE_TEXT_ANTIALIAS_OFF);
+			writeIntFromObj(renderSettings, storage, "Dither", RenderingHints.VALUE_DITHER_ENABLE, RenderingHints.VALUE_DITHER_DISABLE);
+			writeIntFromObj(renderSettings, storage, "Antialias", RenderingHints.VALUE_ANTIALIAS_ON, RenderingHints.VALUE_ANTIALIAS_OFF);
+			writeIntFromObj(renderSettings, storage, "FactionalMetrics", RenderingHints.VALUE_FRACTIONALMETRICS_ON, RenderingHints.VALUE_FRACTIONALMETRICS_OFF);
+			writeIntFromObj(renderSettings, storage, "AlphaInterpolation", RenderingHints.VALUE_ALPHA_INTERPOLATION_QUALITY, RenderingHints.VALUE_ALPHA_INTERPOLATION_SPEED);
+			writeIntFromObj(renderSettings, storage, "ColorRenderQuality", RenderingHints.VALUE_COLOR_RENDER_QUALITY, RenderingHints.VALUE_COLOR_RENDER_SPEED);
+			writeIntFromObj(renderSettings, storage, "Stroke", RenderingHints.VALUE_STROKE_PURE, RenderingHints.VALUE_STROKE_NORMALIZE);
 			output.writeObject(storage);
-			
 			output.close();
-		}catch (IOException e){
-			e.printStackTrace();
-		}
+			storage = null;
+		}catch (IOException e){}
 	}
 
 	private static void putAllIfAbsent(HashMap<String, Object> to, HashMap<String, Object> from) {
@@ -77,10 +114,10 @@ public class Settings implements Serializable
 	}
 
 	public Object getValue(String key) {
-		return storage.get(key);
+		return renderSettings.get(key);
 	}
 
 	public void setValue(String key, Object value) {
-		storage.put(key, value);
+		renderSettings.put(key, value);
 	}
 }
